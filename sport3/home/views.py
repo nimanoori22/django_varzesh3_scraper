a45227cdfa5b1de208a318d3cdfa708973435c22
nimanoori22 <nimanoori000@gmail.com>
2020-08-26 17:07:25 +0430

    added templates

--- sport3/home/views.py
+++ sport3/home/views.py
@@ -2,3 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 
+def homeview(request):
+    return render(request, 'index.html')
--- sport3/sport3/settings.py
+++ sport3/sport3/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'news',
     'ckeditor',
+    'home',
 ]
 
 MIDDLEWARE = [
