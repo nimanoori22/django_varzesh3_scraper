18ef1495e104a0cd794ef9040148f68c8edcead9
nimanoori22 <nimanoori000@gmail.com>
2020-08-26 17:50:45 +0430

    added css and js

--- sport3/sport3/urls.py
+++ sport3/sport3/urls.py
@@ -22,7 +22,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.homeview, name='home'),
-] + static(
-    settings.STATIC_URL, 
-    document_root=settings.STATIC_ROOT,
-)
+]
